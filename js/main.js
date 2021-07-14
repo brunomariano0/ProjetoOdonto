0c1147f02d1436f7c5b2236243deec9193b2b9fd
Bruno Mariano <brunopmariano@outlook.com.br>
2021-07-14 11:34:49 -0300

    subindo

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+
+var usuarios = [
+    {"login": "bcf", "senha": "bcf"},
+    {"login": "mamae", "senha": "abacaxi"},
+    {"login": "papai", "senha": "melancia"},
+];
+
+function Login() {
+    var usuario = document.getElementsByName('username')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value;
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            window.location = "index.html";
+            return true;
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+    return false;
+}
+
